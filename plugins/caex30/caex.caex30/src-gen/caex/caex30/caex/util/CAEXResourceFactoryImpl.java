45adb161d39a73ded2dff111b447e84fb841a54c
Manuel <wimmer@big.tuwien.ac.at>
2017-10-13 11:57:29 +0200

    Added XML Import Export Files

---
+++ plugins/caex30/caex.caex30/src-gen/caex/caex30/caex/util/CAEXResourceFactoryImpl.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Business Informatics Group at TU Wien
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Manuel Wimmer and Emanuel Maetzler
+ *******************************************************************************/
+/**
+ */
+package caex.caex30.caex.util;
+
+import caex.caex30.caex.CAEXPackage;
+
+import org.eclipse.emf.common.util.URI;
+
+import org.eclipse.emf.ecore.EPackage;
+
+import org.eclipse.emf.ecore.impl.EPackageRegistryImpl;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+import org.eclipse.emf.ecore.resource.impl.ResourceFactoryImpl;
+
+import org.eclipse.emf.ecore.util.BasicExtendedMetaData;
+import org.eclipse.emf.ecore.util.ExtendedMetaData;
+
+import org.eclipse.emf.ecore.xmi.XMLResource;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource Factory</b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see CAEX.util.CAEXResourceImpl
+ * @generated
+ */
+public class CAEXResourceFactoryImpl extends ResourceFactoryImpl {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ExtendedMetaData extendedMetaData;
+
+	/**
+	 * Creates an instance of the resource factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public CAEXResourceFactoryImpl() {
+		super();
+		extendedMetaData = new BasicExtendedMetaData(new EPackageRegistryImpl(EPackage.Registry.INSTANCE));
+		extendedMetaData.putPackage(null, CAEXPackage.eINSTANCE);
+	}
+
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Resource createResource(URI uri) {
+		XMLResource result = new CAEXResourceImpl(uri);
+		result.getDefaultSaveOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, extendedMetaData);
+		result.getDefaultLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, extendedMetaData);
+
+		result.getDefaultSaveOptions().put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
+
+		result.getDefaultLoadOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
+		result.getDefaultSaveOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
+
+		result.getDefaultLoadOptions().put(XMLResource.OPTION_USE_LEXICAL_HANDLER, Boolean.TRUE);
+		return result;
+	}
+
+} //CAEXResourceFactoryImpl
---
+++ plugins/caex30/caex.caex30/src-gen/caex/caex30/caex/util/CaexSerializer.java
@@ -0,0 +1,71 @@
+package caex.caex30.caex.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.BasicExtendedMetaData;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.util.ExtendedMetaData;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+
+import caex.caex30.caex.CAEXPackage;
+
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 Vienna University of Technology.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License v1.0 which accompanies 
+ * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Alexander Bergmayr - initial API and implementation
+ *
+ *******************************************************************************/
+
+
+public class CaexSerializer {
+	
+	public void writeXML(Resource xmiResource, String xmlFile) throws IOException {		
+		// create the resource set and register the metamodel packages		
+	
+		xmiResource.getResourceSet().getPackageRegistry().put(CAEXPackage.eINSTANCE.getNsURI(), CAEXPackage.eINSTANCE);
+		
+		final ExtendedMetaData extendedMetaData = new BasicExtendedMetaData(xmiResource.getResourceSet().getPackageRegistry());
+		// put metadata options
+		Map<Object,Object> options = new HashMap<Object,Object>();
+		options.put(XMLResource.OPTION_EXTENDED_META_DATA, extendedMetaData);
+		
+		URI xmlURI = URI.createFileURI(new File(xmlFile).getAbsolutePath());
+		CAEXResourceImpl caexResource = new CAEXResourceImpl(xmiResource.getURI());
+		
+		caexResource.getContents().add(EcoreUtil.copy(xmiResource.getContents().get(0)));
+		caexResource.setURI(xmlURI);
+		caexResource.save(options);
+		
+	}
+	
+	public void writeXMI(File xmlFile, ResourceSet resourceSet, String modelFile) throws IOException {
+		FileInputStream stream = new FileInputStream(xmlFile);
+		
+		CAEXXMLProcessor processor = new CAEXXMLProcessor();
+		
+		final ExtendedMetaData extendedMetaData = new BasicExtendedMetaData(processor.getEPackageRegistry());
+		// put metadata options
+		Map<Object,Object> options = new HashMap<Object,Object>();
+		options.put(XMLResource.OPTION_EXTENDED_META_DATA, extendedMetaData);
+		
+		Resource amlResource = processor.load(stream, options);
+		
+		URI modelURI = URI.createFileURI(new File(modelFile).getAbsolutePath());
+		Resource xmiResource = resourceSet.createResource(modelURI);
+		xmiResource.getContents().add(EcoreUtil.copy(amlResource.getContents().get(0)));
+		
+		xmiResource.save(null);
+	}
+
+}
